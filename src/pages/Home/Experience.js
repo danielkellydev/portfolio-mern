6b8199d6a259f766e15f8ca620b8935a44840ae0
danielkellydev <danielkellydev@gmail.com>
2023-06-08 12:24:12 +1000

    completed structure of experience-section

--- src/pages/Home/Experience.js
+++ src/pages/Home/Experience.js
@@ -8,8 +8,8 @@ const [selectedItemIndex, setSelectedItemIndex] = React.useState(0)
     <div>
         <SectionTitle title="Experience"/>
 
-        <div className='flex py-10'>
-            <div className='flex flex-col gap-10 border-l-2 border-white'>
+        <div className='flex py-20 gap-20 flex-col lg:flex-row'>
+            <div className='flex flex-row lg:flex-col gap-10 border-l-2 border-white lg:w-1/3 overflow-x-scroll'>
             {experience.map((experience, index) => 
                 <div onClick={()=> {
                     setSelectedItemIndex(index)
@@ -19,7 +19,11 @@ const [selectedItemIndex, setSelectedItemIndex] = React.useState(0)
                 </div>
             )}
             </div>
-            <div>
+
+            <div className='flex flex-col gap-5'>
+                <h1 className= 'text-secondary text-xl'>{experience[selectedItemIndex].title}</h1>
+                <h1 className= 'text-tertiary text-xl'>{experience[selectedItemIndex].company}</h1> 
+                <p className= 'text-white text-lg'>{experience[selectedItemIndex].description}</p>
         </div>
         </div>
     </div>
--- src/resources/experience.js
+++ src/resources/experience.js
@@ -15,7 +15,7 @@ export const experience = [
     company: 'Coding Job',
     title: 'Freelance developer',
     period: '2023-Present',
-    description: 'Description'
+    description: 'Successfully managed and self-promoted my own business for six years Consulted with clients to discuss their health issues and prescribed specific herbal remedies Maintained and updated the business\'s bookings and e-commerce website (Wordpress) Developed rapport with clients, fostering an environment of trust and safety to discuss sensitive issues Formulate plans, working iteratively to achieve client\'s goals'
 }
 
 ]
